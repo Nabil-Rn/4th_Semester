567161f87b1683b3739ac7ae0f919da99eb4cef1
Nabil-Rn <98413966+Nabil-Rn@users.noreply.github.com>
2023-10-13 11:41:23 -0400

    week 8
    database in flutter!

---
+++ AppDev2/Dart_Files/week8/model.dart
@@ -0,0 +1,32 @@
+class Planets {
+  late final int id;
+  late final String name;
+  late final int age;
+  late final int distancefromsun;
+
+  Planets(
+      {required this.id,
+      required this.name,
+      required this.age,
+      required this.distancefromsun});
+
+  // Sqlite uses maps consisting of key column names and their values
+  // to communicate with the database. ToMap() and FromMap() methods
+  // changes these maps into objects and objects to maps
+
+
+  Planets.fromMap(Map<String, dynamic> result)
+      : id = result["id"],
+        name = result["name"],
+        age = result["age"],
+        distancefromsun = result["distancefromsun"];
+
+  Map<String, Object> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'age': age,
+      'distancefromsun': distancefromsun
+    };
+  }
+}
